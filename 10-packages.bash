7814f94f73989c0083e86767a186eada3ae8edbd
Adam Price <komidore64@gmail.com>
2016-01-11 10:01:00 -0500

    turn tabs on for shell scripts

--- 10-packages.bash
+++ 10-packages.bash
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
+# vim: noet
 
 PKG_GROUPS=(
-    [0]="Development Tools"
+	[0]="Development Tools"
 )
 
 PACKAGES=(
-    vim
-    git
-    tig
-    tmux
-    mosh
-    koji
-    rpmdevtools
-    ack
+	vim
+	git
+	tig
+	tmux
+	mosh
+	koji
+	rpmdevtools
+	ack
 )
 
 function main () {
-    local i
+	local i
 
-    sudo yum update -y
+	sudo yum update -y
 
-    for i in $( seq 0 $(( ${#PKG_GROUPS[@]} - 1 )) ); do
-        sudo yum groupinstall -y ${PKG_GROUPS[${i}]}
-    done
+	for i in $( seq 0 $(( ${#PKG_GROUPS[@]} - 1 )) ); do
+		sudo yum groupinstall -y ${PKG_GROUPS[${i}]}
+	done
 
-    for i in $( seq 0 $(( ${#PACKAGES[@]} - 1 )) ); do
-        sudo yum install -y ${PACKAGES[${i}]}
-    done
+	for i in $( seq 0 $(( ${#PACKAGES[@]} - 1 )) ); do
+		sudo yum install -y ${PACKAGES[${i}]}
+	done
 }
 
 set -x
